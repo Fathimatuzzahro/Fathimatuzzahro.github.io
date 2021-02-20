749a0ae9110fc19c748455c18ba414b105b2b48a
Fathimatuzzahro <zahro010323@gmail.com>
2021-02-20 22:31:59 +0700

    initial commit

---
+++ include/data.php
@@ -0,0 +1,11 @@
+<?php
+
+// kalo di java kan nulisnya, misal int nama variabel = nilai;
+// kalo di php, dollar ($)nama variabel = nilai (boleh apa aja);
+// $angka = 123;
+// $desimal = 23.54;
+// $boolean = true;
+
+$navTitle = 'Study Club Web';
+
+?>
